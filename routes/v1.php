d81cc23eb879cbce3c4896a4fa02216781f72943
starkwang <wsd312@163.com>
2020-04-27 19:45:21 +0800

    feat: init

---
+++ routes/v1.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//Route::post('login', 'V1\AuthController@login')->name('login');
+
+//登录不使用中间件
+// Route::namespace('V1')->middleware('no.auth')->group(function(){
+//     Route::post('login', 'AuthController@login');
+// });
+
+// Route::namespace('V1')->middleware('refresh.token')->group(function () {
+//     Route::get('ding', 'UserController@ding');
+// });
+// //权限验证
+// Route::namespace('V1')->middleware('role.control')->group(function(){
+
+// });
